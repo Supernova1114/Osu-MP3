7c27913c6d0a21064242df73c5beb0654b1cc4b1
Supernova1114 <supernovawarriors128@gmail.com>
2020-12-26 22:35:22 -0800

    Created Project

---
+++ src/sample/MD5Calculator.java
@@ -0,0 +1,71 @@
+package sample;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MD5Calculator {
+
+
+    private static String getFileChecksum(MessageDigest digest, File file) throws IOException
+    {
+        //Get file input stream for reading the file content
+        FileInputStream fis = new FileInputStream(file);
+
+        //Create byte array to read data in chunks
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+
+        //Read file data and update in message digest
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        };
+
+        //close the stream; We don't need it now.
+        fis.close();
+
+        //Get the hash's bytes
+        byte[] bytes = digest.digest();
+
+        //This bytes[] has bytes in decimal format;
+        //Convert it to hexadecimal format
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i< bytes.length ;i++)
+        {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        //return complete hash
+        return sb.toString();
+    }
+
+    public static void GetMD5Hash(File file) throws NoSuchAlgorithmException, IOException {
+        //Create checksum for this file
+        file = new File("D:\\Program Files\\osu!\\Songs\\532522 SakiZ - osu!memories\\SakiZ - osu!memories (DeRandom Otaku) [Happy Memories].osu");
+
+        //Use MD5 algorithm
+        MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+
+        //Get the checksum
+        String checksum = getFileChecksum(md5Digest, file);
+
+        //see checksum
+        System.out.println(checksum);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
