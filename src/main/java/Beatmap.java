c61cefd999d9f8c9cf100f67ea8d938906155b21
Cameron Rosenthal <supernovawarriors128@gmail.com>
2025-01-10 22:22:11 -0800

    Working beatmap serializing and deserializing

--- src/main/java/Beatmap.java
+++ src/main/java/Beatmap.java
@@ -1,21 +1,92 @@
+import org.json.JSONObject;
+
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 
 public class Beatmap {
 
     private String filePath;
 
+    private long fileLastModified;
+
     private String MD5Hash;
 
     private SongData songData;
 
-    public Beatmap(String filePath, String MD5Hash, SongData songData) {
+
+    public Beatmap(String filePath, long fileLastModified, String MD5Hash, SongData songData) {
         this.filePath = filePath;
+        this.fileLastModified = fileLastModified;
         this.MD5Hash = MD5Hash;
         this.songData = songData;
     }
 
-    public static Beatmap parseBeatmapFile(File beatmapFile) {
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public long getFileLastModified() {
+        return fileLastModified;
+    }
+
+    public String getMD5Hash() {
+        return MD5Hash;
+    }
+
+    public SongData getSongData() {
+        return songData;
+    }
+
+    private static String[] parseKeyValue(String line) {
+
+        String delimiter = ":";
+        String[] split = line.split(delimiter);
+
+        String[] result = new String[2];
+        result[0] = split[0].trim(); // Key
+        result[1] = split.length > 1 ? split[1].trim() : ""; // Value
+
+        return result;
+    }
+
+    public JSONObject serializeBeatmap() {
+        JSONObject beatmapObj = new JSONObject();
+        JSONObject songDataObj = new JSONObject();
+
+        songDataObj.put("songName", songData.songName);
+        songDataObj.put("artistName", songData.artistName);
+        songDataObj.put("filePath", songData.filePath);
+
+        beatmapObj.put("filePath", filePath);
+        beatmapObj.put("fileLastModified", fileLastModified);
+        beatmapObj.put("hash", MD5Hash);
+        beatmapObj.put("songData", songDataObj);
+
+        return beatmapObj;
+    }
+
+    public static Beatmap deserializeBeatmap(JSONObject beatmapObj) {
+
+        JSONObject songDataObj = beatmapObj.getJSONObject("songData");
+
+        SongData songData = new SongData();
+        songData.filePath = songDataObj.getString("filePath");
+        songData.songName = songDataObj.getString("songName");
+        songData.artistName = songDataObj.getString("artistName");
+
+        Beatmap beatmap = new Beatmap(
+                beatmapObj.getString("filePath"),
+                beatmapObj.getLong("fileLastModified"),
+                beatmapObj.getString("hash"),
+                songData
+        );
+
+        return beatmap;
+    }
+
+    public static Beatmap deserializeBeatmap(File beatmapFile) {
 
         SongData songData = new SongData();
 
@@ -55,32 +126,19 @@ public class Beatmap {
             e.printStackTrace();
         }
 
-        // TODO - properly handle failed parsing / incomplete song data.
-        return new Beatmap(beatmapFile.getPath(), hash, songData);
+        // TODO - properly handle failed parsing / incomplete song data???
+        return new Beatmap(beatmapFile.getPath(), beatmapFile.lastModified(), hash, songData);
     }
 
-    public String getFilePath() {
-        return filePath;
+    @Override
+    public String toString() {
+        return "Beatmap:\n" +
+                "filePath = " + filePath + "\n" +
+                "fileLastModified = " + fileLastModified + "\n" +
+                "MD5Hash = " + MD5Hash + "\n" +
+                "songName = " + songData.songName + "\n" +
+                "artistName = " + songData.artistName + "\n" +
+                "songFilePath = " + songData.filePath + "\n";
+
     }
-
-    public String getMD5Hash() {
-        return MD5Hash;
-    }
-
-    public SongData getSongData() {
-        return songData;
-    }
-
-    private static String[] parseKeyValue(String line) {
-
-        String delimiter = ":";
-        String[] split = line.split(delimiter);
-
-        String[] result = new String[2];
-        result[0] = split[0].trim(); // Key
-        result[1] = split.length > 1 ? split[1].trim() : ""; // Value
-
-        return result;
-    }
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,50 @@
+import org.json.JSONObject;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
     public static void main(final String[] args) throws InterruptedException {
-        App.launch(App.class, args);
+        //App.launch(App.class, args);
+
+
+        test();
+    }
+
+    public static void test() {
+        File testFile = new File("D:\\Program Files\\osu!\\Songs\\357777 NOMA - Brain Power\\NOMA - Brain Power (Jacob) [Exote's EXHAUST].osu");
+
+        Beatmap beatmap = Beatmap.deserializeBeatmap(testFile);
+        System.out.println(beatmap);
+
+        Path path = Path.of("test.db");
+
+        try {
+            Files.deleteIfExists(path);
+            Files.createFile(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("test.db"))) {
+            bufferedWriter.write(beatmap.serializeBeatmap().toString(4));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String dbPath = "D:\\Projects\\Osu-MP3\\test.db";
+        try {
+
+            JSONObject beatmapObj = new JSONObject(new String(Files.readAllBytes(Path.of(dbPath))));
+            Beatmap beatmap2 = Beatmap.deserializeBeatmap(beatmapObj);
+
+            System.out.println(beatmap2);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 }
